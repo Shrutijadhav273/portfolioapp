5eb6869f26e708206e77977c38b40b795d1611b8
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-03-24 16:57:02 +0530

    signup  page functionality added

--- app/src/main/java/com/example/portfolioapp128/ui/screens/SignupScreen.kt
+++ app/src/main/java/com/example/portfolioapp128/ui/screens/SignupScreen.kt
@@ -1,4 +1,64 @@
 package com.example.portfolioapp128.ui.screens
 
-class SignupScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun SignupScreen(onNavigateToLogin: () -> Unit) {
+
+    val auth = FirebaseAuth.getInstance()
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var message by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text("Sign Up", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        message = if (it.isSuccessful) "Signup Success" else "Signup Failed"
+                    }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Sign Up")
+        }
+
+        Text(message)
+
+        TextButton(onClick = onNavigateToLogin) {
+            Text("Already have an account? Login")
+        }
+    }
 }

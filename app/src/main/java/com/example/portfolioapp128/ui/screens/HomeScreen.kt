5dc595834a4e2b97982399802986e75f77941eab
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-03-25 14:49:45 +0530

    msg displayed on home screen

--- app/src/main/java/com/example/portfolioapp128/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/portfolioapp128/ui/screens/HomeScreen.kt
@@ -1,4 +1,9 @@
 package com.example.portfolioapp128.ui.screens
 
-class HomeScreen {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Welcome to Home Screen 🎉")
 }
